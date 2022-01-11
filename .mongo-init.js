dbfda73a306769e7425af49744fcbd4b9b609d30
RuthNjeri6 <rthmbugua@gmail.com>
2022-01-11 12:09:43 +0300

    upto date script

---
+++ .mongo-init.js
@@ -0,0 +1,8 @@
+db.createUser(
+{
+        user: "main",
+        pwd:  "question123", 
+        roles: [ { role: "readWrite", db: "aq" },
+                 { role: "readWrite", db: "bm" },
+        ]
+})
